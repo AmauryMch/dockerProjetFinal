dd2e862a0e3be8db8c1d7df3f556520fa7a66be7
IceK37 <45316097+IceK37@users.noreply.github.com>
2025-11-20 11:01:46 +0100

    nginx conf

--- spring-api/src/main/java/fr/digicrafters/simple/rest/app/api/ItemController.java
+++ spring-api/src/main/java/fr/digicrafters/simple/rest/app/api/ItemController.java
@@ -5,7 +5,6 @@ import fr.digicrafters.simple.rest.app.domain.ItemRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*")
 @RequestMapping("/api/items")
 public class ItemController {
     private static final Logger log = LoggerFactory.getLogger(ItemController.class);
